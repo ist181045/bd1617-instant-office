4a1e6ef2abcecc0b7634e2c16b9aeb6296b1c738
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2016-12-15 17:00:12 +0000

    Change some type sizes

--- stage04/reservations_star.sql
+++ stage04/reservations_star.sql
@@ -16,25 +16,25 @@ DROP TABLE if exists olap_Time_dim;
 SOURCE populate_procs.sql;
 
 CREATE TABLE olap_User_dim (
-  user_id  INTEGER(10) UNIQUE NOT NULL AUTO_INCREMENT,
+  user_id  INTEGER(9)  NOT NULL UNIQUE AUTO_INCREMENT,
   nif      VARCHAR(9)  NOT NULL,
   name     VARCHAR(80) NOT NULL,
   phone    VARCHAR(26) NOT NULL,
   PRIMARY KEY(user_id)
 );
-CALL load_user_dim;
+CALL load_user_dim();
 
 CREATE TABLE olap_Location_dim (
-  location_id      INTEGER(10)  UNIQUE NOT NULL,
+  location_id      INTEGER(9)   NOT NULL UNIQUE AUTO_INCREMENT,
   code_office      VARCHAR(255),
-  code_space       VARCHAR(255) NOT NULL,
+  code_space       VARCHAR(255),
   address_building VARCHAR(255) NOT NULL,
   PRIMARY KEY(location_id)
 );
-CALL load_location_dim;
+CALL load_location_dim();
 
 CREATE TABLE olap_Date_dim (
-  date_id       INTEGER(8) UNIQUE NOT NULL,
+  date_id       INTEGER(3) NOT NULL UNIQUE AUTO_INCREMENT,
   date_day      INTEGER(2) NOT NULL,
   date_week     INTEGER(2) NOT NULL,
   date_month    INTEGER(2) NOT NULL,
@@ -42,21 +42,21 @@ CREATE TABLE olap_Date_dim (
   date_year     INTEGER(4) NOT NULL,
   PRIMARY KEY(date_id)
 );
-CALL load_date_dim;
+CALL load_date_dim();
 
 CREATE TABLE olap_Time_dim (
-  time_id     INTEGER(10) UNIQUE NOT NULL,
+  time_id     INTEGER(3)  NOT NULL UNIQUE AUTO_INCREMENT,
   time_minute INTEGER(2)  NOT NULL,
   time_hour   INTEGER(2)  NOT NULL,
   PRIMARY KEY(time_id)
 );
-CALL load_time_dim;
+CALL load_time_dim();
 
 CREATE TABLE olap_Reservations (
-  location_id INTEGER(10)   NOT NULL,
-  time_id     INTEGER(10)   NOT NULL,
-  date_id     INTEGER(10)   NOT NULL,
-  user_id     INTEGER(10)   NOT NULL,
+  location_id INTEGER(9)    NOT NULL,
+  time_id     INTEGER(3)    NOT NULL,
+  date_id     INTEGER(3)    NOT NULL,
+  user_id     INTEGER(9)    NOT NULL,
   amount      DECIMAL(19,4) NOT NULL,
   time_period INTEGER(3)    NOT NULL,
   PRIMARY KEY(location_id, time_id, date_id),
@@ -65,4 +65,4 @@ CREATE TABLE olap_Reservations (
   FOREIGN KEY(date_id)     REFERENCES olap_Date_dim(date_id),
   FOREIGN KEY(user_id)     REFERENCES olap_User_dim(user_id)
 );
-CALL load_reservations;
+CALL load_reservations();
